df0ca14115b6512ae3a6a8333b553676005b02ec
hodorsec <hodorsec@protonmail.com>
2019-02-19 21:03:01 +0100

    Modified script to include HEX formatted strings

--- 0_helpers_scripts/string_to_hex_and_asmhex.py
+++ 0_helpers_scripts/string_to_hex_and_asmhex.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python
+# Author: hodorsec
+# Version 0.6
+# - Added hex encoded variant
+
 import sys
 
 if len(sys.argv) != 2:
@@ -10,10 +14,18 @@ input = sys.argv[1]
 
 print 'String length : ' +str(len(input)) + '\n'
 
+
 print "Converted [{opcode} {0x hex} ; {reversed string}] format"
 stringList = [input[i:i+4] for i in range(0, len(input), 4)]
 for item in stringList[::-1] :
 	print 'push 0x' + str(item[::-1].encode('hex')) + ' ; ' + item[::-1]
+
+print ""
+
+print "Hex encoded opcode  //  PUSH 0x{string}] format"
+stringList = [input[i:i+4] for i in range(0, len(input), 4)]
+for item in stringList[::-1]:
+    print "\"\\x68\\x" + "\\x".join("{:02x}".format(ord(c)) for c in item.encode('utf-8')) + "\"\t// " + 'push 0x' + str(item[::-1].encode('hex'))
 
 print ""
 
@@ -26,4 +38,5 @@ for item in stringList[::-1] :
 asmList = reversed(asmList)
 
 print(", ".join(str(i) for i in asmList))
+
 
