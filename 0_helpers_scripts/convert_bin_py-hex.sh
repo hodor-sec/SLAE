263b9272125514ab0cd8bbdfb8594c3741f05d42
hodorsec <hodorsec@protonmail.com>
2019-03-06 15:41:37 +0100

    Added XOR encoder/decoder and modified BIN to PY conversion file

--- 0_helpers_scripts/convert_bin_py-hex.sh
+++ 0_helpers_scripts/convert_bin_py-hex.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 if [ $# -eq 1 ]; then
-            ndisasm ./$1 -b 32 | awk '{printf "%s\t# %s %s #\n", $2, $3, $4;}' | sed 's/[0-9A-F]\{2\}/\\x&/g' | sed 's/^/\"/g' | sed 's/\t/\"&/' | column -t
-            else echo "Give a binary program as argument."
+	objdump -D -m i386 -M intel -b binary ./$1 | tail -n +8 | sed 's/^.*[0-9a-f]:\t/ /' | sed 's/[0-9a-f]\{2\} /\\x&/g' | sed 's/ \\x/\\x/g' | sed 's/^/\"/g' | awk '{print $1"\"\t\t# " $2 " " $3 " " $4}' | column -t
+        else echo "Give a binary program as argument."
 fi
+
